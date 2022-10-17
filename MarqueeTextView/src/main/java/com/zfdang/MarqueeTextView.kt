aa174a727f8d83db337db097756ef5e5a877bbd5
dantifer <dantifer@gmail.com>
2022-10-17 17:12:24 +0800

    initial commit

---
+++ MarqueeTextView/src/main/java/com/zfdang/MarqueeTextView.kt
@@ -0,0 +1,408 @@
+package com.zfdang
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.ColorInt
+import androidx.annotation.FloatRange
+import androidx.annotation.Px
+import java.lang.ref.WeakReference
+import kotlin.math.abs
+import kotlin.math.ceil
+
+/**
+ * 文本横向滚动，跑马灯效果
+ * - 设置的内容不要太长，建议不要超过 [String] 最大长度的 1/5
+ * @author JianXin
+ * @date 2021-07-22 11:59
+ */
+open class MarqueeTextView @JvmOverloads constructor(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    companion object {
+        const val TAG = "MarqueeTextView"
+        const val BLANK = " "
+        const val REPEAT_SINGLE = 1 //一次结束
+        const val REPEAT_SINGLE_LOOP = 0 //单个循序
+        const val REPEAT_FILL_LOOP = -1 // 填充后循环
+    }
+
+    /***
+     * 滚动速度
+     */
+    var speed = 1f
+        set(value) {
+            if (value <= 0) {
+                field = 0f
+                stop()
+            } else {
+                field = value
+            }
+        }
+
+    /**
+     * 文本内容
+     */
+    var text = ""
+        set(value) {
+            if (field.isEmpty() && value.isEmpty()) {
+                return
+            }
+            field = value
+            var targetContent = value
+            if (isResetLocation) { //控制重新设置文本内容的时候，是否初始化xLocation。
+                xLocation = width * startLocationDistance
+            }
+            val itemEndBlank = getItemEndBlank()
+            if (!targetContent.endsWith(itemEndBlank)) {
+                targetContent += itemEndBlank //避免没有后缀, 补充空格位
+            }
+            //计算宽度，根据模式来
+            if (repeat == REPEAT_FILL_LOOP) {
+                mFinalDrawText = ""
+                //计算文本的宽度
+                mSingleContentWidth = getTextWidth(targetContent)
+                if (mSingleContentWidth > 0) {
+                    // 最大可见内容项数
+                    val maxVisibleCount = ceil(width / mSingleContentWidth.toDouble()).toInt() + 1
+                    repeat(maxVisibleCount) {
+                        mFinalDrawText += targetContent
+                    }
+                }
+                contentWidth = getTextWidth(mFinalDrawText)
+            } else {
+                if (xLocation < 0 && repeat == REPEAT_SINGLE) {
+                    if (abs(xLocation) > contentWidth) {
+                        xLocation = width * startLocationDistance
+                    }
+                }
+                mFinalDrawText = targetContent
+                contentWidth = getTextWidth(mFinalDrawText)
+                mSingleContentWidth = contentWidth
+            }
+            textHeight = getTextHeight()
+            invalidate()
+        }
+
+    /**
+     * 最终绘制显示的文本
+     */
+    private var mFinalDrawText: String = ""
+
+    /**
+     * 文字颜色
+     */
+    @ColorInt
+    var textColor = Color.BLACK
+        set(value) {
+            if (value != field) {
+                field = value
+                textPaint.color = textColor
+                invalidate()
+            }
+        }
+
+    /**
+     * 字体大小
+     */
+    @Px
+    var textSize = 12f
+        set(value) {
+            if (value > 0 && value != field) {
+                field = value
+                textPaint.textSize = value
+                if (text.isNotEmpty()) {
+                    text = text
+                }
+            }
+        }
+
+    /**item间距，*/
+    @Px
+    var textItemDistance = 50f
+        set(value) {
+            if (field == value) {
+                return
+            }
+            field = if (value < 0f) 0f else value
+            if (text.isNotEmpty()) {
+                text = text
+            }
+        }
+
+    /**
+     * 滚动模式
+     */
+    var repeat = REPEAT_SINGLE_LOOP
+        set(value) {
+            if (value != field) {
+                field = value
+                resetInit = true
+                text = text
+            }
+        }
+
+    /**
+     * 开始的位置距离左边，0~1，0:最左边，1:最右边，0.5:中间。
+     */
+    @FloatRange(from = 0.0, to = 1.0)
+    var startLocationDistance = 0.0f
+        set(value) {
+            if (value == field) {
+                return
+            }
+            field = when {
+                value < 0f -> 0f
+                value > 1f -> 1f
+                else -> value
+            }
+        }
+
+    /**
+     * 是否重置文本绘制的位置，默认为true
+     */
+    var isResetLocation = true
+    private var xLocation = 0f //文本的x坐标
+
+    /**
+     * 单个显示内容的宽度
+     */
+    private var mSingleContentWidth: Float = 0f
+
+    /**内容的宽度*/
+    private var contentWidth = 0f
+
+    /**是否继续滚动*/
+    var isRolling = false
+        private set
+
+    /**画笔*/
+    protected val textPaint: TextPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
+
+    private var textHeight = 0f
+    private var resetInit = true
+
+    private val mHandler by lazy { MyHandler(this) }
+
+    /**
+     * 是否用户主动调用，默认 true
+     */
+    var isRollByUser = true
+
+    init {
+        initAttrs(attrs)
+        initPaint()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        if (resetInit && text.isNotEmpty()) {
+            textItemDistance = textItemDistance
+            xLocation = width * startLocationDistance
+            resetInit = false
+        }
+        val absLocation = abs(xLocation)
+        when (repeat) {
+            REPEAT_SINGLE -> if (contentWidth < absLocation) {
+                stop()
+            }
+            REPEAT_SINGLE_LOOP -> if (contentWidth <= absLocation) {
+                //一轮结束
+                xLocation = width.toFloat()
+            }
+            REPEAT_FILL_LOOP -> if (xLocation < 0 && mSingleContentWidth <= absLocation) {
+                xLocation = mSingleContentWidth - absLocation
+            }
+            else ->
+                if (contentWidth < absLocation) {
+                    //也就是说文字已经到头了
+                    stop()
+                }
+        }
+        //绘制文本
+        if (mFinalDrawText.isNotBlank()) {
+            canvas.drawText(mFinalDrawText, xLocation, height / 2 + textHeight / 2, textPaint)
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        if (!isRollByUser) {
+            startInternal(true)
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        if (isRolling) {
+            stopInternal(false)
+        }
+        super.onDetachedFromWindow()
+    }
+
+    override fun setVisibility(visibility: Int) {
+        super.setVisibility(visibility)
+        if (visibility != VISIBLE) {
+            stopInternal(false)
+        } else {
+            if (!isRollByUser) {
+                startInternal(false)
+            }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        text = text
+    }
+
+    private fun initAttrs(attrs: AttributeSet?) {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.MarqueeTextView)
+        textColor = a.getColor(R.styleable.MarqueeTextView_android_textColor, textColor)
+        isResetLocation = a.getBoolean(R.styleable.MarqueeTextView_marqueeResetLocation, true)
+        speed = a.getFloat(R.styleable.MarqueeTextView_marqueeSpeed, 1f)
+        textSize = a.getDimension(R.styleable.MarqueeTextView_android_textSize, 12f)
+        textItemDistance = a.getDimension(R.styleable.MarqueeTextView_marqueeItemDistance, 50f)
+        startLocationDistance = a.getFloat(
+            R.styleable.MarqueeTextView_marqueeStartLocationDistance,
+            0f
+        )
+        repeat = a.getInt(R.styleable.MarqueeTextView_marqueeRepeat, REPEAT_SINGLE_LOOP)
+        text = a.getText(R.styleable.MarqueeTextView_android_text)?.toString() ?: ""
+        a.recycle()
+    }
+
+    /**
+     * 刻字机修改
+     */
+    private fun initPaint() {
+        textPaint.apply {
+            style = Paint.Style.FILL
+            color = textColor
+            textSize = textSize
+            isAntiAlias = true
+            density = 1 / resources.displayMetrics.density
+        }
+    }
+
+    /**
+     * 切换开始暂停
+     */
+    fun toggle() {
+        if (isRolling) {
+            stop()
+        } else {
+            start()
+        }
+    }
+
+    /**
+     * 继续滚动
+     */
+    fun start() {
+        startInternal(true)
+    }
+
+    /**
+     * [isRollByUser] 是否用户主动调用
+     */
+    protected fun startInternal(isRollByUser: Boolean) {
+        this.isRollByUser = isRollByUser
+        stop()
+        if (text.isNotBlank()) {
+            mHandler.sendEmptyMessage(MyHandler.WHAT_RUN)
+            isRolling = true
+        }
+    }
+
+    /**
+     * 停止滚动
+     */
+    fun stop() {
+        stopInternal(true)
+    }
+
+    /**
+     * [isRollByUser] 是否用户主动调用
+     */
+    protected fun stopInternal(isRollByUser: Boolean) {
+        this.isRollByUser = isRollByUser
+        isRolling = false
+        mHandler.removeMessages(MyHandler.WHAT_RUN)
+    }
+
+    /**
+     * 计算出一个空格的宽度
+     * @return
+     */
+    private fun getBlankWidth(): Float {
+        return getTextWidth(BLANK)
+    }
+
+    private fun getTextWidth(text: String?): Float {
+        if (text.isNullOrEmpty()) {
+            return 0f
+        }
+        return textPaint.measureText(text)
+    }
+
+    /**
+     * 文本高度
+     */
+    private fun getTextHeight(): Float {
+        val fontMetrics = textPaint.fontMetrics
+        return abs(fontMetrics.bottom - fontMetrics.top) / 2
+    }
+
+    private fun getItemEndBlank(): String {
+        val oneBlankWidth = getBlankWidth() //空格的宽度
+        var count = 1
+        if (textItemDistance > 0 && oneBlankWidth != 0f) {
+            count = (ceil(textItemDistance / oneBlankWidth).toInt()) //粗略计算空格数量
+        }
+        val builder = StringBuilder(count)
+        for (i in 0..count) {
+            builder.append(BLANK)//间隔字符串
+        }
+        return builder.toString()
+    }
+
+    /**
+     * handler
+     *
+     * @author JianXin
+     * @date 2021-07-22 11:43
+     */
+    private class MyHandler(view: MarqueeTextView) : Handler(Looper.getMainLooper()) {
+
+        companion object {
+            internal const val WHAT_RUN = 1001
+        }
+
+        private val mRef = WeakReference(view)
+
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            if (msg.what == WHAT_RUN) {
+                mRef.get()?.apply {
+                    if (speed > 0) {
+                        xLocation -= speed
+                        invalidate()
+                        // 10 毫秒绘制一次
+                        sendEmptyMessageDelayed(WHAT_RUN, 10)
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/zfdang/mbb/FullscreenActivity.java
@@ -0,0 +1,124 @@
+package com.zfdang.mbb;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.zfdang.MarqueeTextView;
+import com.zfdang.mbb.databinding.ActivityFullscreenBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+
+    private MarqueeTextView mContentView;
+
+    private final Handler mHideHandler = new Handler(Looper.myLooper());
+
+    private ActivityFullscreenBinding binding;
+
+    private FloatingActionButton setting;
+    private Typeface typeface;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mContentView = binding.fullscreenContent;
+        setting = binding.fabSetting;
+        typeface = Typeface.createFromAsset(getAssets(), "Ruthie.ttf");
+
+        // show billboard in landscape mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+
+        // add floating button for config page
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                mContentView.setSpeed(mContentView.getSpeed()+1);
+//                mContentView.setTypeface(typeface);
+            }
+        });
+
+        mContentView.start();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been created
+        delayedHide(100);
+    }
+
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        // mHideRunnable --> mHidePart2Runnable
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            // Hide UI first
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
+            // Schedule a runnable to remove the status and navigation bar after a delay
+            mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+        }
+    };
+
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+            if (Build.VERSION.SDK_INT >= 30) {
+                mContentView.getWindowInsetsController().hide(
+                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            } else {
+                // Note that some of these constants are new as of API 16 (Jelly Bean)
+                // and API 19 (KitKat). It is safe to use them, as they are inlined
+                // at compile-time and do nothing on earlier devices.
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            }
+        }
+    };
+
+}
